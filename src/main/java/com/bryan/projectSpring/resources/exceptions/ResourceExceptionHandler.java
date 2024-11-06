346293e7737aa50a4c59f21636eb7119a82ccc93
Bryan <bryan.gps18@gmail.com>
2024-11-06 14:45:19 -0300

    Exception handling - delete

--- src/main/java/com/bryan/projectSpring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/bryan/projectSpring/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.bryan.projectSpring.resources.exceptions;
 
+import com.bryan.projectSpring.services.exceptions.DatabaseException;
 import com.bryan.projectSpring.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -19,4 +20,13 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), resquest.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest resquest){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), resquest.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
--- src/main/java/com/bryan/projectSpring/services/UserService.java
+++ src/main/java/com/bryan/projectSpring/services/UserService.java
@@ -2,9 +2,11 @@ package com.bryan.projectSpring.services;
 
 import com.bryan.projectSpring.entities.User;
 import com.bryan.projectSpring.repositories.UserRepository;
-import com.bryan.projectSpring.resources.exceptions.ResourceExceptionHandler;
+import com.bryan.projectSpring.services.exceptions.DatabaseException;
 import com.bryan.projectSpring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,7 +32,16 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+         repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(e.getMessage());
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj){
