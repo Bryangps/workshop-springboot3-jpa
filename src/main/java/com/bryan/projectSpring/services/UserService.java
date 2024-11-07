147d5abb29706cdfca4a696d3797f4e7046713c4
Bryan <bryan.gps18@gmail.com>
2024-11-07 07:53:56 -0300

    Exception handling - update

--- src/main/java/com/bryan/projectSpring/services/UserService.java
+++ src/main/java/com/bryan/projectSpring/services/UserService.java
@@ -4,6 +4,7 @@ import com.bryan.projectSpring.entities.User;
 import com.bryan.projectSpring.repositories.UserRepository;
 import com.bryan.projectSpring.services.exceptions.DatabaseException;
 import com.bryan.projectSpring.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -36,7 +37,7 @@ public class UserService {
          repository.deleteById(id);
         }
         catch (EmptyResultDataAccessException e){
-            throw new ResourceNotFoundException(e.getMessage());
+            throw new ResourceNotFoundException(id);
         }
         catch (DataIntegrityViolationException e){
             throw new DatabaseException(e.getMessage());
@@ -45,9 +46,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj){
